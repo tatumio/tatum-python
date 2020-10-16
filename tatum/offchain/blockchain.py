c4e7606320c2917cac96ca28ae443592b0680f84
Žaneta <zaneta.1996@seznam.cz>
2020-10-16 09:19:21 +0200

    offchain done

--- tatum/offchain/blockchain.py
+++ tatum/offchain/blockchain.py
@@ -75,3 +75,118 @@ def send_ethereum_from_tatum_ledger_to_blockchain(body_params):
     res = conn.getresponse()
     data = res.read()
     print(data.decode("utf-8"))    
+
+def create_new_ERC20_token(body_params):
+    validator.create_new_ERC20_token(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+
+    conn.request("POST", "/v3/offchain/ethereum/erc20", body_params, headers)
+
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))    
+
+def deploy_ethereum_erc20_smart_contract_offchain(body_params):
+    validator.deploy_ethereum_erc20_smart_contract_offchain(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+
+    conn.request("POST", "/v3/offchain/ethereum/erc20/deploy", body_params, headers)
+
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))   
+
+def set_erc20_token_contract_address(path_params):
+    validator.set_erc20_token_contract_address(path_params)
+    headers = { 'x-api-key': API_KEY }
+
+    conn.request("POST", "/v3/offchain/ethereum/erc20/{}/{}".format(path_params['name'], path_params['address']), headers=headers)
+
+#   _______________________________________________________________
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+
+def transfer_ethereum_erc20_from_tatum_ledger_to_blockchain(body_params):
+    validator.transfer_ethereum_erc20_from_tatum_ledger_to_blockchain(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+
+    conn.request("POST", "/v3/offchain/ethereum/erc20/transfer", body_params, headers)
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))  
+
+def send_xlm_asset_from_tatum_ledger_to_blockchain(body_params):
+    validator.send_xlm_asset_from_tatum_ledger_to_blockchain(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+    conn.request("POST", "/v3/offchain/xlm/transfer", body_params, headers)
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+def create_xlm_based_asset(body_params):
+    validator.create_xlm_based_asset(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+    conn.request("POST", "/v3/offchain/xlm/asset", body_params, headers)
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+def send_xrp_from_tatum_ledger_to_blockchain(body_params):
+    validator.send_xrp_from_tatum_ledger_to_blockchain(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+    conn.request("POST", "/v3/offchain/xrp/transfer", body_params, headers)
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+def create_xrp_based_asset(body_params):
+    validator.create_xrp_based_asset(body_params)
+    body_params = json.dumps(body_params)
+    headers = {
+        'content-type': "application/json",
+        'x-api-key': API_KEY
+        }
+    conn.request("POST", "/v3/offchain/xrp/asset", body_params, headers)
+#   _______________________________________________________________
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
--- validator.py
+++ validator.py
@@ -554,3 +554,161 @@ def send_ethereum_from_tatum_ledger_to_blockchain(body_params):
         check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'amount', body_params['amount'])
     if 'fee' in body_params.keys():
         check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'fee', body_params['fee'])
+
+def create_new_ERC20_token(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "symbol": {"required": True, "type" : "string", "minlength": 1, "maxlength": 30},
+            "supply" : {"required": True, "type" : "string", "minlength": 1, "maxlength": 38},
+            "description" : {"required": True, "type" : "string", "minlength": 1, "maxlength": 100},
+            "basePair" : {"required": True, "type" : "string", "minlength": 3, "maxlength": 5},
+            "customer": {"type" : "dict", "schema": {'externalId': {"required": True, "type" : "string", "minlength": 1, "maxlength": 100}, "providerCountry": {"type" : "string", "minlength": 2, "maxlength": 2}, "customerCountry": {"type" : "string", "minlength": 2, "maxlength": 2},           "accountingCurrency": {"type" : "string", "minlength": 3, "maxlength": 3} }},
+            "accountingCurrency": {"type" : "string", "minlength": 3, "maxlength": 3},
+            "derivationIndex": {"type": "integer", "max": 4294967295},
+            "xpub": {"type" : "string", "minlength": 1, "maxlength": 150},
+            "address": {"type" : "string", "minlength": 42, "maxlength": 42}
+         }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[a-zA-Z0-9_]+$', 'symbol', body_params['symbol'])
+    check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'supply', body_params['supply'])   
+
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BAT", "BBD", "BCH", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "ETH", "EUR", "FJD", "FKP", "FREE", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LEO", "LINK", "LKR", "LRD", "LSL", "LTC", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MKR", "MMK", "MMY", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PAX", "PAXG", "PEN", "PGK", "PHP", "PKR", "PLN", "PLTC", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TUSD", "TWD", "TZS", "UAH", "UGX", "USD", "USDC", "USDT", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XCON", "XDR", "XLM", "XOF", "XPF", "XRP", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
+    check_correct_value_from_define_list(currencies, 'basePair', body_params['basePair'])
+    if 'customer' in body_params.keys():
+        if 'accountingCurrency' in body_params.keys():
+            check_correct_value_from_define_list(currencies, 'accountingCurrency', body_params['customer']['accountingCurrency'])
+    if 'accountingCurrency' in body_params.keys():
+        check_correct_value_from_define_list(currencies, 'accountingCurrency', body_params['accountingCurrency'])
+
+
+def deploy_ethereum_erc20_smart_contract_offchain(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "symbol": {"required": True, "type" : "string", "minlength": 1, "maxlength": 30},
+            "supply" : {"required": True, "type" : "string", "minlength": 1, "maxlength": 38},
+            "description" : {"required": True, "type" : "string", "minlength": 1, "maxlength": 100},
+            "basePair" : {"required": True, "type" : "string", "minlength": 2, "maxlength": 30},
+            "customer": {"type" : "dict", "schema": {'externalId': {"required": True, "type" : "string", "minlength": 1, "maxlength": 100}, "providerCountry": {"type" : "string", "minlength": 2, "maxlength": 2}, "customerCountry": {"type" : "string", "minlength": 2, "maxlength": 2},           "accountingCurrency": {"type" : "string", "minlength": 3, "maxlength": 3} }},
+            "xpub": {"type" : "string", "minlength": 1, "maxlength": 150},
+            "derivationIndex": {"type": "integer", "max": 4294967295},
+            "address": {"type" : "string", "minlength": 42, "maxlength": 42},
+            "mnemonic": {"type" : "string", "minlength": 1, "maxlength": 500},
+            "index": {"type": "integer", "max": 4294967295},
+            "privateKey": {"type": "string", "minlength": 66, "maxlength": 66}
+            "signatureId": {"type" : "string", "minlength": 36, "maxlength": 36},
+            "nonce": {"type": "integer", "min": 0}           
+         }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[a-zA-Z0-9_]+$', 'symbol', body_params['symbol'])
+    check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'supply', body_params['supply'])   
+
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BAT", "BBD", "BCH", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "ETH", "EUR", "FJD", "FKP", "FREE", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LEO", "LINK", "LKR", "LRD", "LSL", "LTC", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MKR", "MMK", "MMY", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PAX", "PAXG", "PEN", "PGK", "PHP", "PKR", "PLN", "PLTC", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TUSD", "TWD", "TZS", "UAH", "UGX", "USD", "USDC", "USDT", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XCON", "XDR", "XLM", "XOF", "XPF", "XRP", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
+    if 'customer' in body_params.keys():
+        if 'accountingCurrency' in body_params.keys():
+            check_correct_value_from_define_list(currencies, 'accountingCurrency', body_params['customer']['accountingCurrency'])
+
+def set_erc20_token_contract_address(path_params):
+    v = cerberus.Validator()
+    path_schema = {
+            "address": {"required": True, "type" : "string", "minlength": 1, "maxlength": 100},
+            "name": {"required": True, "type" : "string", "minlength": 1, "maxlength": 30},
+        }
+    v.validate(path_params, path_schema)
+    erros_print(v)
+    check_allowed_chars('^[a-zA-Z0-9_]+$', 'name', path_params['name'])
+
+def transfer_ethereum_erc20_from_tatum_ledger_to_blockchain(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "senderAccountId" : {"required": True, "type" : "string", "minlength": 24, "maxlength": 24},
+            "address": {"required": True, "type" : "string", "minlength": 42, "maxlength": 42},
+            "amount" : {"required": True, "type" : "string", "maxlength": 38},
+            "compliant": {"type": "boolean"},
+            "currency" : {"type" : "string", "minlength": 2, "maxlength": 30},
+            "privateKey": {"type": "string", "minlength": 66, "maxlength": 66},
+            "signatureId": {"type" : "string", "minlength": 36, "maxlength": 36},
+            "mnemonic": {"type" : "string", "minlength": 1, "maxlength": 500},
+            "index": {"type": "integer", "max": 4294967295},
+            "nonce": {"type": "integer", "min": 0},
+            "paymentId":{"type" : "string","minlength": 1, "maxlength": 100},
+            "senderNote":{"type" : "string","minlength": 1, "maxlength": 500}
+        }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'amount', body_params['amount'])
+
+def send_xlm_asset_from_tatum_ledger_to_blockchain(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "senderAccountId" : {"required": True, "type" : "string", "minlength": 24, "maxlength": 24},
+            "fromAccount": {"required": True, "type" : "string", "minlength": 56, "maxlength": 56},
+            "address": {"required": True, "type" : "string", "minlength": 56, "maxlength": 56},
+            "amount" : {"required": True, "type" : "string", "maxlength": 38},
+            "secret": {"type" : "string", "minlength": 56, "maxlength": 56},
+            "signatureId": {"type" : "string", "minlength": 36, "maxlength": 36},
+            "compliant": {"type": "boolean"},
+            "attr": {"type" : "string", "maxlength": 64},
+            "paymentId":{"type" : "string","minlength": 1, "maxlength": 100},
+            "senderNote":{"type" : "string","minlength": 1, "maxlength": 500}
+            "issuerAccount": {"type" : "string", "minlength": 56, "maxlength": 56},
+            "token": {"type" : "string", "minlength": 1, "maxlength": 12}
+        }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'amount', body_params['amount'])
+    if 'attr' in body_params.keys():
+        check_allowed_chars('^[ -~]{0,64}$', 'attr', body_params['attr'])
+    if 'token' in body_params.keys():
+        check_allowed_chars('^[a-zA-Z0-9]{1,12}$', 'token', body_params['token'])
+
+def create_xlm_based_asset(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "issuerAccount": {"required": True, "type" : "string", "minlength": 56, "maxlength": 56},
+            "token": {"required": True, "type" : "string", "minlength": 1, "maxlength": 12},
+            "basePair" : {"required": True, "type" : "string", "minlength": 3, "maxlength": 5}
+        }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[a-zA-Z0-9]{1,12}$', 'token', body_params['token'])
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BAT", "BBD", "BCH", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "ETH", "EUR", "FJD", "FKP", "FREE", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LEO", "LINK", "LKR", "LRD", "LSL", "LTC", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MKR", "MMK", "MMY", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PAX", "PAXG", "PEN", "PGK", "PHP", "PKR", "PLN", "PLTC", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TUSD", "TWD", "TZS", "UAH", "UGX", "USD", "USDC", "USDT", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XCON", "XDR", "XLM", "XOF", "XPF", "XRP", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
+    check_correct_value_from_define_list(currencies, 'basePair', body_params['basePair'])
+
+def send_xrp_from_tatum_ledger_to_blockchain(body_params):
+    v = cerberus.Validator()
+    body_schema = {
+            "senderAccountId" : {"required": True, "type" : "string", "minlength": 24, "maxlength": 24},
+            "account": {"required": True, "type" : "string", "minlength": 1, "maxlength": 100},
+            "address": {"required": True, "type" : "string", "minlength": 1, "maxlength": 100},
+            "amount" : {"required": True, "type" : "string", "maxlength": 38},
+            "compliant": {"type": "boolean"},
+            "attr": {"type" : "string"},
+            "sourceTag": {"type" : "integer"},
+            "paymentId":{"type" : "string","minlength": 1, "maxlength": 100},
+            "secret": {"type" : "string", "minlength": 29, "maxlength": 29},
+            "signatureId": {"type" : "string", "minlength": 36, "maxlength": 36},            
+            "senderNote":{"type" : "string","minlength": 1, "maxlength": 500},
+            "issuerAccount": {"type" : "string", "minlength": 33, "maxlength": 34},
+            "token": {"type" : "string", "minlength": 40, "maxlength": 40}
+        }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[+]?((\d+(\.\d*)?)|(\.\d+))$', 'amount', body_params['amount'])
+    if 'token' in body_params.keys():
+        check_allowed_chars('^[a-zA-Z0-9]{1,12}$', 'token', body_params['token'])
+
+
+def create_xrp_based_asset(body_params):
+            v = cerberus.Validator()
+    body_schema = {
+            "issuerAccount": {"required": True, "type" : "string", "minlength": 33, "maxlength": 34},
+            "token": {"required": True, "type" : "string", "minlength": 40, "maxlength": 40},
+            "basePair" : {"required": True, "type" : "string", "minlength": 3, "maxlength": 5}
+        }
+    v.validate(body_params, body_schema)
+    erros_print(v)
+    check_allowed_chars('^[a-zA-Z0-9]{1,12}$', 'token', body_params['token'])
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BAT", "BBD", "BCH", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "ETH", "EUR", "FJD", "FKP", "FREE", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LEO", "LINK", "LKR", "LRD", "LSL", "LTC", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MKR", "MMK", "MMY", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PAX", "PAXG", "PEN", "PGK", "PHP", "PKR", "PLN", "PLTC", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TUSD", "TWD", "TZS", "UAH", "UGX", "USD", "USDC", "USDT", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XCON", "XDR", "XLM", "XOF", "XPF", "XRP", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
+    check_correct_value_from_define_list(currencies, 'basePair', body_params['basePair'])
